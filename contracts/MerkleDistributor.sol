19c036d8a4fb4d6243ff3eae0b08f14f9edfa158
Chimera <81845287+chimera-defi@users.noreply.github.com>
2021-08-16 18:37:04 +0530

    Update MerkleDistributor.sol
    Make everyone start vesting at the same time

--- contracts/MerkleDistributor.sol
+++ contracts/MerkleDistributor.sol
@@ -1,32 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.6.11;
+pragma solidity =0.7.5;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./interfaces/IMerkleDistributor.sol";
 
-contract MerkleDistributor is IMerkleDistributor {
+contract MerkleDistributor {
     using SafeMath for uint256;
 
-    address public immutable override token;
-    bytes32 public immutable override merkleRoot;
+    address public immutable token;
+    bytes32 public immutable merkleRoot;
     uint256 public immutable vestingPeriod;
 
     uint256 public total_allocated_supply;
+    uint256 public start_time;
 
     mapping(address => uint256) initial_locked;
     mapping(address => uint256) total_claimed;
-    mapping(address => uint256) start_time;
 
     // This is a packed array of booleans.
     mapping(uint256 => uint256) private claimedBitMap;
 
-
-    constructor(address token_, bytes32 merkleRoot_, uint256 vestingPeriod_) public {
+    constructor(
+        address token_,
+        bytes32 merkleRoot_,
+        uint256 vestingPeriod_
+    ) public {
         token = token_;
         merkleRoot = merkleRoot_;
         vestingPeriod = vestingPeriod_;
+        start_time = block.timestamp;
         total_allocated_supply = 0;
     }
 
@@ -45,44 +49,47 @@ contract MerkleDistributor is IMerkleDistributor {
     }
 
     function _allocateShares(address account, uint256 amount) private {
-        require((IERC20(token).balanceOf(address(this)) - (total_allocated_supply + amount)) > 0, 'MerkleDistributor: Inadequate contract balance');
+        require(
+            (IERC20(token).balanceOf(address(this)) - (total_allocated_supply + amount)) > 0,
+            "MerkleDistributor: Inadequate contract balance"
+        );
         initial_locked[account] = amount;
         total_claimed[account] = 0;
-        start_time[account] = block.timestamp;
         total_allocated_supply = total_allocated_supply.add(amount);
     }
 
-    function claimShares(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof) external override {
-        require(!isClaimed(index), 'MerkleDistributor: Drop already claimed.');
+    function claimShares(
+        uint256 index,
+        address account,
+        uint256 amount,
+        bytes32[] calldata merkleProof
+    ) external override {
+        require(!isClaimed(index), "MerkleDistributor: Drop already claimed.");
 
         // Verify the merkle proof.
         bytes32 node = keccak256(abi.encodePacked(index, account, amount));
-        require(MerkleProof.verify(merkleProof, merkleRoot, node), 'MerkleDistributor: Invalid proof.');
+        require(MerkleProof.verify(merkleProof, merkleRoot, node), "MerkleDistributor: Invalid proof.");
 
         // Mark it claimed and begin vesting account.
         _setClaimed(index);
         _allocateShares(account, amount);
+        claim(account);
 
         emit ClaimedShares(index, account, amount);
     }
 
-    function getTotalVestedOf(address account) public view returns (uint) {
-        uint256 start = start_time[account];
+    function getTotalVestedOf(address account) public view returns (uint256) {
         uint256 locked = initial_locked[account];
         uint256 t = block.timestamp;
-        if(t < start){
+        if (t < start_time) {
             return 0;
         }
-        uint256 result =  locked.mul(t.sub(start)).div(vestingPeriod);
-        if(result < locked) {
+        uint256 result = locked.mul(t.sub(start_time)).div(vestingPeriod);
+        if (result < locked) {
             return result;
         } else {
             return locked;
         }
-    }
-
-    function getStartTime(address account) public view returns (uint256) {
-        return start_time[account];
     }
 
     function getTotalClaimed(address account) public view returns (uint256) {
@@ -96,7 +103,7 @@ contract MerkleDistributor is IMerkleDistributor {
     function claim(address account) external override {
         uint256 claimable = getTotalVestedOf(account).sub(total_claimed[account]);
         total_claimed[account] = total_claimed[account].add(claimable);
-        require(IERC20(token).transfer(account, claimable), 'MerkleDistributor: Transfer failed.');
+        require(IERC20(token).transfer(account, claimable), "MerkleDistributor: Transfer failed.");
 
         emit claimed(account, claimable);
     }
